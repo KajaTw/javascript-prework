fb2df3cd182df9dac7b6362afabdbbd1dbdbd7b5
Kaja Twardysko <kayasylvia1@gmail.com>
2022-08-06 14:55:19 +0200

    Create functions

--- js/functions.js
+++ js/functions.js
@@ -35,3 +35,63 @@ if (playerInput == "1") {
   playerMove = "nieznany ruch";
 }
 printMessage("Twój ruch: " + playerMove);
+
+var argComputerMove,
+  argMoveId,
+  argPlayerMove,
+  computerMove,
+  playerInput,
+  playerMove,
+  randomNumber;
+
+/**
+ * getMoveName
+ */
+function getMoveName(argMoveId) {
+  console.log("wywołano funkcję getMoveName z argumentem: " + argMoveId);
+  if (argMoveId == 1) {
+    return "kamień";
+  } else if (argMoveId == 2) {
+    return "nożyce";
+  } else if (argMoveId == 3) {
+    return "papier";
+  } else {
+    printMessage(
+      "Nie znam ruchu o id " + argMoveId + '. Zakładam, że chodziło o "kamień".'
+    );
+    return "kamień";
+  }
+}
+
+/**
+ * displayResult
+ */
+function displayResult(argPlayerMove, argComputerMove) {
+  console.log(
+    "wywołano funkcję displayResults z argumentami: " +
+      argPlayerMove +
+      ", " +
+      argComputerMove
+  );
+  if (argPlayerMove == "papier" && argComputerMove == "kamień") {
+    printMessage("Wygrywasz!");
+  } else if (argPlayerMove == "nożyce" && argComputerMove == "papier") {
+    printMessage("Wygrywasz!");
+  } else if (argPlayerMove == "kamien" && argComputerMove == "nożyce") {
+    printMessage("Wygrywasz!");
+  } else if (argPlayerMove == argComputerMove) {
+    printMessage("Przegrywasz :(");
+  } else {
+    printMessage("Przegrywasz :(");
+  }
+  printMessage("Zagrałem " + argComputerMove + ", a Ty " + argPlayerMove);
+}
+playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
+console.log("wybór ruchu gracza to: " + playerInput);
+playerMove = getMoveName(playerInput);
+console.log("ruch gracza to: " + playerMove);
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log("wylosowana liczba to: " + randomNumber);
+computerMove = getMoveName(randomNumber);
+console.log("ruch komputera to: " + computerMove);
+displayResult(playerMove, computerMove);
